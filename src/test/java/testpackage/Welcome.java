86a69bf4d0f10d0df425b0ca7b3c3b28f6351175
Sheweta <Shewetasrivastav190@gmail.com>
2024-01-13 02:31:46 +0530

    numbertest.java

--- src/test/java/testpackage/Welcome.java
+++ src/test/java/testpackage/Welcome.java
@@ -3,7 +3,7 @@ package testpackage;
 public class Welcome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome in Huge learming.com");
 
 	}
 
